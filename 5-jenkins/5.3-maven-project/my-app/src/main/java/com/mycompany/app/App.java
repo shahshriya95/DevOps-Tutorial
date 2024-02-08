abd1f55c5282362f4aca0e34b80d01891227b4ed
shahshriya95 <155866924+shahshriya95@users.noreply.github.com>
2024-02-08 14:49:50 +0530

    Update App.java

--- 5-jenkins/5.3-maven-project/my-app/src/main/java/com/mycompany/app/App.java
+++ 5-jenkins/5.3-maven-project/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,13 @@
-package com.mycompany.app;
+package com.slmvn.app;
 
 /**
- * Hello world!
+ * hiii!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! From Varun Manik in Simplilearn class on date Jan 13 2024" );
+        System.out.println( "Hello World! From shriya shah in Simplilearn class on date feb 08 2024" );
     }
 }
